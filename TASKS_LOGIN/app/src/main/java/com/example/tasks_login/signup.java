47929f69fbb5c97bb93c1b33b2efe19bab8aaa09
Abhishek <absk.sharma7@gmail.com>
2021-03-22 11:26:58 +0530

    first commit

---
+++ TASKS_LOGIN/app/src/main/java/com/example/tasks_login/signup.java
@@ -0,0 +1,30 @@
+package com.example.tasks_login;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class signup extends Fragment implements View.OnClickListener{
+
+    TextView textView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_signup, container, false);
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
